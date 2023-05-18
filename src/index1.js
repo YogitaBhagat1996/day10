5c44d9268796deadd330e80d03226c32a62f2f0a
YogitaBhagat1996 <133939060+YogitaBhagat1996@users.noreply.github.com>
2023-05-18 14:37:22 +0530

    first commit

---
+++ src/index1.js
@@ -0,0 +1,7 @@
+import {} from "node:fs";
+
+function main() {
+  console.log("Hello NodeJS!! I am modified in b1 branch");
+}
+
+main();
---
+++ src/index2.js
@@ -0,0 +1,15 @@
+function main()
+{
+    let obj={};
+    let obj1={id:1};
+    let obj2={id:1,name:"Yogita"};
+    let obj3={id:1,name:"Yogita",email:"yogi@gmail.com"};
+    let obj4={id:1,name:"Yogita",email:"yogi@gmail.com",mobile:"8087266501"};
+    console.log(obj);
+    console.log(obj1);
+    console.log(obj2);
+    console.log(obj3);
+    console.log(obj4);
+}
+
+main();
